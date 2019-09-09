6047795f4ed62827db9883516a2913d40f7cfe16
Payam <maroufi@kth.se>
2019-09-09 23:00:02 +0200

    Done, simple but need to think

---
+++ Planina/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    /* The idea is to obtain an mathematical algorithm. Google
+    /* the mathematics and write own algorithm */
+
+    long long result = pow((pow(2, N) + 1), 2);
+    cout << result << endl;
+}
