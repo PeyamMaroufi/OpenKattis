a4ba8d020e5fe819b0dc8b9f34e9dce8223c16b9
Payam <maroufi@kth.se>
2019-09-22 21:59:20 +0200

    Two solutions

--- 1.4 Tai's Formula/main.cpp
+++ 1.4 Tai's Formula/main.cpp
@@ -3,6 +3,36 @@
 #include <iomanip>
 
 using namespace std;
+
+/* Solution 1. This one is better*/
+int main()
+{
+    /* Number of pairs */
+    int i;
+    cin >> i;
+
+    if (i > 0)
+    {
+        /* Get the first pair of input */
+        double t, g;
+        cin >> t >> g;
+        double t1, g1, area = 0;
+
+        /* Get the rest of the points and swap the values of g1 and g etc. */
+        for (int z = 1; z < i; z++)
+        {
+            cin >> t1 >> g1;
+            area += ((g1 + g) / 2) * (t1 - t);
+            t = t1;
+            g = g1;
+        }
+
+        /* Output the result */
+        cout << fixed << setprecision(5) << area / 1000 << endl;
+    }
+}
+
+/* Solution 2 */
 int main()
 {
     int numOfSamples_i;
